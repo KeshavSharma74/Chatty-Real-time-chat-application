155c44a0dbaa3c497cf6505e60b2fdeb0fa85ec5
keshavsharma74 <keshav165p@gmail.com>
2025-08-11 19:48:36 +0530

    Created auth middleware

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import protectRoute from "../middlewares/auth.middleware.js";
 
 const authRoute = Router();
 
 authRoute.post('/signup',signup);
 authRoute.post('/login',login);
 authRoute.post('/logout',logout);
+authRoute.post('/update-profile',protectRoute,updateProfile);
+authRoute.get('/check-auth',protectRoute,checkAuth);
+
 
 export default authRoute;
