1541e5aeb059892b8926ff69bdda7da46df1d5ba
keshavsharma74 <keshav165p@gmail.com>
2025-08-13 23:29:37 +0530

    Updated deployed backend url

--- client/src/libs/axios.js
+++ client/src/libs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"https://chatty-real-time-chat-application.vercel.app",
+  baseURL:"https://chatty-real-time-chat-application-4s51.onrender.com",
   withCredentials: true,
 });
--- client/src/store/useAuthStore.jsx
+++ client/src/store/useAuthStore.jsx
@@ -3,7 +3,7 @@ import { axiosInstance } from "../libs/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "https://chatty-real-time-chat-application.vercel.app";
+const BASE_URL = "https://chatty-real-time-chat-application-4s51.onrender.com";
 // const BASE_URL = "http://localhost:3000";
 
 export const useAuthStore = create( (set,get)=> ({
