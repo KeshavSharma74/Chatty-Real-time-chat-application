395234bd7ec5ab38ec8544074f38fc26420d55f6
keshavsharma74 <keshav165p@gmail.com>
2025-08-13 16:10:10 +0530

    Updated Frontend Url

--- server/libs/socket.js
+++ server/libs/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: ["https://chatty-real-time-chat-application-7.vercel.app"],
   },
 });
 
